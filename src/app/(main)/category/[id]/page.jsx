b801101adff6284cc7ededc2f1109478942616cf
mursahaulIslamNoor <mursahaulnur17@gmail.com>
2026-04-28 12:11:43 +0530

    added the lib folder to add all the data fecting function from the api

--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -1,25 +1,15 @@
 import LeftSideBar from '@/components/homepage/news/LeftSideBar';
 import RightSideBar from '@/components/homepage/news/RightSideBar';
+import { getCategories, getNewsByCategoryId } from '@/lib/data';
 import React from 'react';
 
 
-async function getCategories() {
-    const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
-    const data = await res.json()
-    return data.data;
-}
-
-async function getNewsByCategoryId(catetory_id) {
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${catetory_id}`);
-    const data = await res.json()
-    return data.data;
-}
 
 
 // to get the id of the page we use server components "params"
 const NewsCategoryPage = async ({ params }) => {
     const { id } = await params;
-    console.log(id)
+    // console.log(id)
 
     const categories = await getCategories()
     // console.log(categories.news_category, "object categories")
@@ -35,9 +25,9 @@ const NewsCategoryPage = async ({ params }) => {
             </div>
 
             {/* All news section */}
-            <div className="font-bold bg-amber-200 col-span-6">
-                <h1>All News</h1>
-                <div className="space-y-4">
+            <div className="font-bold col-span-6">
+                <h1 className=''>News By Category</h1>
+                <div className="space-y-4 mt-6">
 
                     {
                         news.length > 0 ? news.map(n => {
---
+++ src/lib/data.js
@@ -0,0 +1,11 @@
+export async function getCategories() {
+    const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+    const data = await res.json()
+    return data.data;
+}
+
+export async function getNewsByCategoryId(catetory_id) {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${catetory_id}`);
+    const data = await res.json()
+    return data.data;
+}
